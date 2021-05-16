5a161af085bd147bfc905b380a9cbb298f72b6e3
mateusleao <mateusleao81@gmail.com>
2021-05-15 21:11:49 -0500

    python kafka exercises, plus spark repo

---
+++ 1-kafka-faust-python/1-kafka_and_python/3-producer-config/1.3.producer_config.py
@@ -0,0 +1,105 @@
+import asyncio
+from dataclasses import dataclass, field
+import json
+import random
+
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+from faker import Faker
+
+
+faker = Faker()
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+TOPIC_NAME = "org.udacity.exercise3.purchases"
+
+
+@dataclass
+class Purchase:
+    username: str = field(default_factory=faker.user_name)
+    currency: str = field(default_factory=faker.currency_code)
+    amount: int = field(default_factory=lambda: random.randint(100, 200000))
+
+    def serialize(self):
+        """Serializes the object in JSON string format"""
+        # TODO: Serializer the Purchase object
+        #       See: https://docs.python.org/3/library/json.html#json.dumps
+        return json.dumps(
+            {
+                "username": self.username,
+                "currency": self.currency,
+                "amount": self.amount,
+            }
+        )
+
+
+async def produce_sync(topic_name):
+    """Produces data synchronously into the Kafka Topic"""
+    p = Producer(
+        {
+            "bootstrap.servers": BROKER_URL,
+            "compression.type" : "lz4",  # other opts: gzip, snappy, zstd
+            "linger.ms" : "500", # this is the default value
+            "batch.num.messages" : "10000", # this first 2 is what makes it go to kafka, first importance
+            "queue.buffering.max.messages" : "1000000", # this is secondary and independent
+            "queue.buffering.max.kbytes" : "1048576" # this is secondary and independent, if above is not met this one won't be able to happen
+            })
+
+    # TODO: Write a synchronous production loop.
+    #       See: https://docs.confluent.io/current/clients/confluent-kafka-python/#confluent_kafka.Producer.flush
+    while True:
+        # TODO: Instantiate a `Purchase` on every iteration. Make sure to serialize it before
+        #       sending it to Kafka!
+        p.produce(topic_name, Purchase().serialize())
+        p.flush()
+        # Do not delete this!
+        await asyncio.sleep(0.01)
+
+        
+def main():
+    """Checks for topic and creates the topic if it does not exist"""
+    create_topic(TOPIC_NAME)
+    try:
+        asyncio.run(produce_consume())
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+    
+async def produce_consume():
+    """Runs the Producer and Consumer tasks"""
+    t1 = asyncio.create_task(produce_sync(TOPIC_NAME))
+    t2 = asyncio.create_task(_consume(TOPIC_NAME))
+    await t1
+    await t2
+
+    
+async def _consume(topic_name):
+    """Consumes produced messages"""
+    c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0"})
+    c.subscribe([topic_name])
+    num_consumed=0
+    while True:
+        msg = c.consume(timeout=0.001)
+        if msg:
+            num_consumed += 1
+            if num_consumed % 100 == 0:
+                print(f"consumed {num_consumed} messages")
+        else:
+            await asyncio.sleep(0.01)
+
+        
+def create_topic(client):
+    """Creates the topic with the given topic name"""
+    client = AdminClient({"bootstrap.servers": BROKER_URL})
+    futures = client.create_topics(
+        [NewTopic(topic=TOPIC_NAME, num_partitions=5, replication_factor=1)]
+    )
+    for _, future in futures.items():
+        try:
+            future.result()
+        except Exception as e:
+            print("exiting production loop")
+
+
+if __name__ == "__main__":
+    main()
